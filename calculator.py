2334c1417638824b61442e9d970cf00491c3ab9f
bradyDarren <dcgbfinancial@gmail.com>
2024-10-16 14:21:43 -0500

    addition of calculator function

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-
+# creation of functions designed to perform the basic functions of a calculator. 
 def add (n1, n2):
     return (n1 + n2)
 
@@ -11,7 +11,30 @@ def multiply (n1, n2):
 def divide (n1, n2):
     return (n1 / n2)
 
-print(add(10, 5))
-print(subtract(10, 5))
-print(multiply(10, 5))
-print(divide(10, 5))
+calc_functions = {
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide
+}
+
+continue_calc = True
+
+def calculator():
+    number1 = int(input("What is the first number?: "))
+    
+    while continue_calc:
+        for symbol in calc_functions: 
+            print(symbol)
+        symbol_choice = input("Pick an operation: ")
+        number2 = int(input("What's the next number?: "))
+        result = float(calc_functions[symbol_choice](number1,number2))
+        print(f"{number1} + {number2} = {result}")
+        user_choice = input(f"Type 'y' to continue caculating with {result}, or type 'n' to start a new calculation: ")
+        if user_choice == 'n':
+            print("\n" * 40)
+            calculator()
+        else:
+            number1 = result 
+            continue
+calculator()
