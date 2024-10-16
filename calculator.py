7611f5afed46e5eea1f88a88b1414dac969736c8
bradyDarren <dcgbfinancial@gmail.com>
2024-10-16 06:30:42 -0500

    creation of 4 function needed on a calculator.

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+def add (n1, n2):
+    return (n1 + n2)
+
+def subtract (n1, n2):
+    return (n1 - n2)
+
+def multiply (n1, n2):
+    return (n1 * n2)
+
+def divide (n1, n2):
+    return (n1 / n2)
+
+print(add(10, 5))
+print(subtract(10, 5))
+print(multiply(10, 5))
+print(divide(10, 5))
